70f537207e584347dfca0d8d56fca2352e0c5738
ArsenalBAck2004 <utphuongmap@gmail.com>
2024-07-12 16:02:30 -0400

    add the game page with logo file

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchInput = document.getElementById('search-input');
+  const searchButton = document.getElementById('search-button');
+  const gamesGrid = document.getElementById('games-grid');
+  const categoryButtons = document.querySelectorAll('.categories button');
+  const playButtons = document.querySelectorAll('.play-button');
+
+});
