719e37db49da7b3ec482289d152d2c0780fe6f1b
birongliu <birongliu2003@gmail.com>
2024-07-19 09:06:22 -0400

    feat: Add community router to handle API requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import "dotenv/config"
+import community from "./community/index"
 const app = express();
+app.use(express.json())
 
-
-
+app.use("/api/community", community)
 
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
