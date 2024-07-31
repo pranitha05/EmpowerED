83b822485ecad5fbd6dbc7b74c9e1b062254227e
birongliu <birongliu2003@gmail.com>
2024-07-31 19:24:07 -0400

    chore: Update .env.example and validation.js with required environment variables

--- src/utilities/validation.js
+++ src/utilities/validation.js
@@ -1,14 +1,21 @@
 import z from "zod";
 
 export const envValidator = z.object({
-    PORT: z.string(),
-    FRONTEND_URL: z.string(),
-    SESSION_SECRET: z.string()
-})
+  PORT: z.string(),
+  FRONTEND_URL: z.string(),
+  SESSION_SECRET: z.string(),
+  JWT_TOKEN_SECRET: z.string(),
+  FRONTEND_URL: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_USER: z.string(),
+  DATABASE_PORT: z.string(),
+  DATABASE_NAME: z.string(),
+});
 
 export const registerValidator = z.object({
-    email: z.string(),
-    password: z.string(),
-    firstName: z.string(),
-    lastName: z.string(),
-  });
+  email: z.string(),
+  password: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+});
